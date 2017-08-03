b24b891609b776a2786974e558b98713b644a91e
vitalijlogvinenko <mirenrich@gmail.com>
2017-08-03 10:12:41 +0300

    release v1.0.1

--- scripts/release.js
+++ scripts/release.js
@@ -19,4 +19,7 @@ console.log('=> Pushing tag');
 publishUtils.exec('git push origin ' + TAG_NAME);
 
 console.log('=> Publish');
-publishUtils.exec('yarn build && npm publish --tag next');
+publishUtils.exec('yarn build');
+console.log('cd lib');
+shell.cd('lib');
+publishUtils.exec('npm publish --tag next');
